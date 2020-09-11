138dc23144693e8294a891276e4df0a3bff89514
unknown <evelyn.depiala@gmail.com>
2020-09-10 22:51:58 -0300

    atualização

--- scripts/addField.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#add-time")
-    .addEventListener('click', clonerField)
-
-function clonerField() {
-    const fields = document.querySelector('.schudule-item').cloneNode(true)
-    document.querySelector('#schedule-items').appendChild(fields)
-}
